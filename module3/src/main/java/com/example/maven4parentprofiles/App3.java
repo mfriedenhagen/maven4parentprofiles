f3d54578d05f935031b0217f90fcb1f5e2fb1255
Volker Füßler <volker.fuessler@1und1.de>
2025-04-23 13:24:21 +0200

    chore: module 3 and 4 that also use docker profile

---
+++ module3/src/main/java/com/example/maven4parentprofiles/App3.java
@@ -0,0 +1,10 @@
+package com.example.maven4parentprofiles;
+
+/**
+ * Hello world!
+ */
+public class App3 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ module3/src/test/java/com/example/maven4parentprofiles/App3Test.java
@@ -0,0 +1,19 @@
+package com.example.maven4parentprofiles;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class App3Test {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
